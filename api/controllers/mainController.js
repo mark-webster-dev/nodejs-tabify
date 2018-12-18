974e61805bd81698bec0778b2e93741eba751909
mark webster <mark.webster@vocalink.com>
2018-12-18 11:10:17 +0000

    added error handling and event logging

--- api/controllers/mainController.js
+++ api/controllers/mainController.js
@@ -3,6 +3,7 @@
 var tabify = require('es-tabify');
 
 exports.tabify = function(req, res) {
+    console.log("%s:  Processing request from: %s", new Date() , req.connection.remoteAddress);
     var data = tabify(req.body);  
     res.send(data);
 };
--- server.js
+++ server.js
@@ -3,12 +3,33 @@ var express = require('express'),
   bodyParser = require('body-parser'),
   port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-var routes = require('./api/routes/mainRoute'); 
-routes(app); 
+  var title = 'VCS Elasticsearch Tabify API Server';
 
-app.listen(port);
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
 
-console.log('Elasticsearch Tabify API server started on: ' + port);
+  var routes = require('./api/routes/mainRoute'); 
+  routes(app); 
+
+//Default Error Handling
+  app.use((req, res, next) => {
+    const error = new Error('Not Found!');
+    error.status = 404;
+    next(error);
+  });
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+
+//app.listen(port);
+  const server = app.listen(port, (error) => {
+    if (error) return console.log('Error starting %s:%s', title, error);
+
+    console.log('%s listening on port %s', title,  port);
+  });
